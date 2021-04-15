07392c06ef7d3aa04eca2a6a685406d2a5d00237
IngSantana <wilkinsantana23@gmail.com>
2021-04-15 01:37:54 -0400

    Se agregaron las Prefrencias de usuario, grebar token en el storage del dispositivo

--- form_validation/lib/src/providers/usuario_provider.dart
+++ form_validation/lib/src/providers/usuario_provider.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
+import 'package:form_validation/src/preferencias_usuario/preferencias_usuarios.dart';
 import 'package:http/http.dart' as http;
 
 class UsuarioProvider {
   final String _firebaseToken = 'AIzaSyDEb1TapNHX1AY5bvOh3HMR8i3QE5CRIUk';
+  final _prefs = new PreferenciasUsuario();
 
   Future<Map<String, dynamic>> login(String email, String password) async {
     final authData = {
@@ -21,7 +23,7 @@ class UsuarioProvider {
     print(decodedResp);
 
     if (decodedResp.containsKey('idToken')) {
-      //TODO: Salvar el token en el storage
+      _prefs.token = decodedResp['idToken'];
       return {'ok': true, 'token': decodedResp['idToken']};
     } else {
       return {'ok': false, 'mensaje': decodedResp['error']['message']};
@@ -45,7 +47,7 @@ class UsuarioProvider {
     print(decodedResp);
 
     if (decodedResp.containsKey('idToken')) {
-      //TODO: Salvar el token en el storage
+      _prefs.token = decodedResp['idToken'];
       return {'ok': true, 'token': decodedResp['idToken']};
     } else {
       return {'ok': false, 'mensaje': decodedResp['error']['message']};
